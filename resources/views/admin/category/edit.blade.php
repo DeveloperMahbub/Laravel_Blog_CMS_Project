635bcfcae8f81615949c67f6ee1435ac9841f98e
Mahbubur Rahman <mahbubur2310@gmail.com>
2022-08-09 02:29:04 +0600

    Blog Category Edit and Update  Complete

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -11,24 +11,32 @@
             <div class="card">
                 <div class="header">
                     <h2>
-                        EDIT TAG
+                        EDIT CATEGORY
                     </h2>
                 </div>
                 <div class="body">
-                    <form action="{{ route('admin.tag.update',$tag->id) }}" method="POST">
+                    <form action="{{ route('admin.category.update',$category->id) }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
                         <div class="form-group form-float">
                             <div class="form-line">
                                 <input type="text" id="name" 
                                 name="name"
-                                value="{{ $tag->name }}"
+                                value="{{ $category->name }}"
                                 class="form-control">
-                                <label class="form-label">Tag Name</label>
+                                <label class="form-label">Category Name</label>
+                            </div>
+                        </div>
+                        <div class="form-group form-float">
+                            <div class="form-line">
+                                <input type="file" id="image" 
+                                name="image"
+                                class="form-control">
+                                <label class="form-label">Category Image</label>
                             </div>
                         </div>
                         <br>
-                        <a class="btn btn-danger m-t-15 waves-effect" href="{{ route('admin.tag.index') }}">BACK</a>
+                        <a class="btn btn-danger m-t-15 waves-effect" href="{{ route('admin.category.index') }}">BACK</a>
                         <button type="submit" class="btn btn-primary m-t-15 waves-effect">UPDATE</button>
                     </form>
                 </div>
