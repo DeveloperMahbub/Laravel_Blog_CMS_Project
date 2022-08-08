db4469bc7d057ac5c48f627a6d1a2e9d0f15b5a2
Mahbubur Rahman <mahbubur2310@gmail.com>
2022-08-09 03:22:09 +0600

    Many to Many Relationship Between Posts Categories and Tags  Complete

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo('App\Models\Role');
     }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Models\Post');
+    }
 }
