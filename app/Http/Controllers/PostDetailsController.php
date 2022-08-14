ab1328020574aacbc12322df21b1fa489460e3ee
Mahbubur Rahman <mahbubur2310@gmail.com>
2022-08-14 12:53:06 +0600

    Show All Post And View Count Complete

--- app/Http/Controllers/PostDetailsController.php
+++ app/Http/Controllers/PostDetailsController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class PostDetailsController extends Controller
 {
+
+    public function index()
+    {
+        $posts = Post::latest()->paginate(12);
+        return view('allPosts',compact('posts'));
+    }
+
     public function details($slug)
     {
        $posts = Post::where('slug',$slug)->first();
+       $blogKey = 'blog_'.$posts->id;
+       if (!Session::has($blogKey)) {
+        $posts->increment('view_count');
+        Session::put($blogKey,1);
+    }
         $randomPosts = Post::all()->random(3);
         return view('postDetails',compact('posts','randomPosts'));
     }
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use Illuminate\Support\Facades\Route;
 // })->name('home');
 Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('/post/{slug}',[PostDetailsController::class,'details'])->name('post.details');
+Route::get('/posts',[PostDetailsController::class,'index'])->name('post.index');
 
 // Route::middleware([
 //     'auth:sanctum',
